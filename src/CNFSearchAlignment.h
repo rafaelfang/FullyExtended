841ea9068f69b53a0cdf727434ce17e273df54be
rafaelfang <cf797@mail.missouri.edu>
2015-11-04 23:59:20 -0600

    CNFSearchAlignment

---
+++ src/CNFSearchAlignment.h
@@ -0,0 +1,20 @@
+/*
+ * CNFSearchAlignment.h
+ *
+ *  Created on: Oct 26, 2015
+ *      Author: Chao
+ */
+
+#ifndef CNFSEARCHALIGNMENT_H_
+#define CNFSEARCHALIGNMENT_H_
+
+#include "Alignment.h"
+
+class CNFSearchAlignment: public Alignment {
+public:
+	CNFSearchAlignment();
+	virtual ~CNFSearchAlignment();
+
+};
+
+#endif /* CNFSEARCHALIGNMENT_H_ */
